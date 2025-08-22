333ee6ee3bdfb9407621a4cc7be3ce19a41c8d4e
Tolga <123948207+tolgapp@users.noreply.github.com>
2025-08-22 09:27:27 +0200

    updated allowedorigins on deployed project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import router from './routes/routing';
@@ -10,17 +10,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002;
-const allowedOrigins = process.env.FRONTEND_LINK ? process.env.FRONTEND_LINK.split(',') : [];
-const url = process.env.MONGO_URI;
+const allowedOrigins = process.env.FRONTEND_LINK?.split(", ");
 
 mongoose
-  .connect(url as string)
+  .connect(process.env.MONGO_URI as string)
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.log('MongoDB connection error', error));
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(requestLogger);
+app.use(cookieParser());
 app.use(
   cors({
     origin: allowedOrigins,
